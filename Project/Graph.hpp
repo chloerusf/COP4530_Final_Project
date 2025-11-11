9db2a73488ec97f21a2e2bed64f1f23e5d1297ed
bumbl3br33 <kopkauan@gmail.com>
2025-11-10 21:14:39 -0500

    Edits made to the Graph.hpp File, this file should be complete now.

--- Project/Graph.hpp
+++ Project/Graph.hpp
@@ -2,32 +2,42 @@
 #define GRAPH_H
 
 #include "GraphBase.hpp"
+#include <string>
 #include <vector>
+#include <unordered_map>
 using namespace std;
 
+class Edge {
+public:
+    string destLabel; // label of the destination vertex
+    unsigned long weight; // weight of the edge
+
+    Edge(string dest, unsigned long w)
+        : destLabel(dest), weight(w) {}
+};
+
+class Vertex {
+public:
+    string label; // vertex name
+    vector<Edge> edges; // edges connected to this vertex
+
+    Vertex(string l) : label(l) {}
+};
 
 class Graph: public GraphBase {
-    private: 
-        vector <vector<int>> adjList;
-        bool isDirected;
+private: 
+    unordered_map<string, Vertex*> vertices;
 
-        public:
-        Graph(int vertices, bool directed = false);
-        ~Graph();
-        void addVertex(std::string label);
-        void removeVertex(std::string label);
-        void addEdge(std::string label1, std::string label2, unsigned long weight);
-        void removeEdge(std::string label1, std::string label2);
-        unsigned long shortestPath(std::string startLabel, std::string endLabel,
-        std::vector<std::string> &path);
-};
+public:
+    Graph();
+    ~Graph();
 
-class Vertex{
-
-};
-
-class Edge{
-
+    void addVertex(std::string label);
+    void removeVertex(std::string label);
+    void addEdge(std::string label1, std::string label2, unsigned long weight);
+    void removeEdge(std::string label1, std::string label2);
+    unsigned long shortestPath(std::string startLabel, std::string endLabel,
+    std::vector<std::string> &path);
 };
 
 #endif
