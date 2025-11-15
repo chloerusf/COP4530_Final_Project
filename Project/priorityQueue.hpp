d183a11afc994c513821df2fc6d8963f97136aa6
Mark Anjoul <marc.anjoul@gmail.com>
2025-11-15 14:25:53 -0500

    Add PriorityQueue class implementation

---
+++ Project/priorityQueue.hpp
@@ -0,0 +1,24 @@
+#ifndef PRIORITYQUEUE_H
+#define PRIORITYQUEUE_H
+
+#include <vector>
+#include <string>
+
+class PriorityQueue {
+private:
+    // Each element is (distance, label)
+    std::vector<std::pair<unsigned long, std::string>> heap;
+
+    // helper functions
+    void bubbleUp(int index);
+    void bubbleDown(int index);
+
+public:
+    PriorityQueue();
+
+    void push(unsigned long dist, const std::string &label);
+    std::pair<unsigned long, std::string> pop();
+    bool isEmpty() const;
+};
+
+#endif
